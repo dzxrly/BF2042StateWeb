5ce2f16f4a824b4b85630cad00e8992cdc1c7508
dzxrly2 <dzxrly2@gmail.com>
2023-11-24 15:51:32 +0800

    2.1.3+23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -135,11 +135,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "2700dfcb9b1540776b0896afb9fd46e5
 "icons/windows11/Wide310x150Logo.scale-150.png": "f4d3a39a97ccd673ac2ee7607e68a8a5",
 "icons/windows11/Wide310x150Logo.scale-200.png": "2c885372b3f1b97a72989ad5a39db1e9",
 "icons/windows11/Wide310x150Logo.scale-400.png": "38040261eb191e9257be5ee49a200d52",
-"index.html": "0691d370a612a31591f96ecaed0e09de",
-"/": "0691d370a612a31591f96ecaed0e09de",
-"main.dart.js": "8282a0ee9e60c4a01ba0919e4f800f04",
+"index.html": "7b66b41c0d2aa82218fdd6fdfc1542c7",
+"/": "7b66b41c0d2aa82218fdd6fdfc1542c7",
+"main.dart.js": "07b0a7cf18e3ae75ea482d33e83a0545",
 "manifest.json": "bb3b029592260d57545edc70693cb992",
-"version.json": "55b5167c4ff9e5c0b3ead8965827b138"};
+"version.json": "6b9867897b5cf6fa98ad4d76aa84ab02"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
